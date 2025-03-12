acc76b9373d2288a96693f1879b77cb133778bca
laveshbansal <43352960+laveshbansal@users.noreply.github.com>
2025-03-12 00:49:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Set view engine to use EJS
 app.set("view engine", "ejs");
